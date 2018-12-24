0d2b31dfe5d596feed86e86c8da5c7fd54be5ad9
Ali Abbas <aliabbas.299792@gmail.com>
2018-12-24 02:43:19 +0000

    Adding notes and cleaning some code/making things a bit more intuitive

--- Ball game/platforms.h
+++ Ball game/platforms.h
@@ -172,7 +172,7 @@ public:
 	~chunksHolder(); //the destructor
 
 	int chunkCoordsWidthHeight[4] = { 0 }; //this will be the virtual chunk that the ball is suppposed to be in, will be used to delete/generate chunks
-	int surroundingChunks[3][3] = { 0 }; //this 3x3 array will represent all the present chunks, and will be updated according to the virtual chunk above
+	bool surroundingChunks[3][3] = { false }; //this 3x3 array will represent all the present chunks, and will be updated according to the virtual chunk above
 
 	void updateChunks(sf::Sprite, int, int); //function to actually delete/create chunks
 };
@@ -204,10 +204,10 @@ void chunksHolder::updateChunks(sf::Sprite sprite1, int winWidth, int winHeight)
 			) {
 			chunksLoaded[i].~chunks(); //destructs the object
 			chunksLoaded.erase(chunksLoaded.begin() + i); //removes the item
-			//std::cout << chunksLoaded.size() << std::endl;
 		}
 	}
 
+	//the below are basically temporary variables to hold the virtual points in each chunk, to check if there is one there
 	int x_sector_0 = chunkCoordsWidthHeight[0] - (chunkCoordsWidthHeight[2] / 2);
 	int x_sector_1 = chunkCoordsWidthHeight[0] + (chunkCoordsWidthHeight[2] / 2);
 	int x_sector_2 = chunkCoordsWidthHeight[0] + (chunkCoordsWidthHeight[2] * 1.5);
@@ -215,6 +215,7 @@ void chunksHolder::updateChunks(sf::Sprite sprite1, int winWidth, int winHeight)
 	int y_sector_1 = chunkCoordsWidthHeight[1] + (chunkCoordsWidthHeight[3] / 2);
 	int y_sector_2 = chunkCoordsWidthHeight[1] + (chunkCoordsWidthHeight[3] * 1.5);
 
+	//populates the surroundingChunks[3][3] array with values corresponding to the presence (or lack thereof) of chunks
 	surroundingChunks[0][0] = chunkOverlap(x_sector_0, y_sector_0);
 	surroundingChunks[0][1] = chunkOverlap(x_sector_1, y_sector_0);
 	surroundingChunks[0][2] = chunkOverlap(x_sector_2, y_sector_0);
@@ -224,19 +225,18 @@ void chunksHolder::updateChunks(sf::Sprite sprite1, int winWidth, int winHeight)
 	surroundingChunks[2][1] = chunkOverlap(x_sector_1, y_sector_2);
 	surroundingChunks[2][2] = chunkOverlap(x_sector_2, y_sector_2);
 
-	//std::cout << chunkCoordsWidthHeight[0] << " -- " << chunkCoordsWidthHeight[1] << std::endl;
-
+	//2 temp variables to hold the x and y coords of any chunks which are to be added
 	int tempCoordX = 0;
 	int tempCoordY = 0;
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			if (i == 1 && j == 1) {
+			if (i == 1 && j == 1) { //continue if on the middle chunk, as there is always one there
 				continue;
 			}
 			
-			if (surroundingChunks[i][j] == 0) {
-				switch (i) {
+			if (surroundingChunks[i][j] == 0) { //if a chunk is not present in where some virtual sector is, further processing
+				switch (i) { //this decides the y coordinate for the chunk, based on the variable i, which belongs to the outer loop, looping down y axis chunks
 				case 0:
 					tempCoordY = chunkCoordsWidthHeight[1] - chunkCoordsWidthHeight[3];
 					break;
@@ -248,7 +248,7 @@ void chunksHolder::updateChunks(sf::Sprite sprite1, int winWidth, int winHeight)
 					break;
 				}
 
-				switch (j) {
+				switch (j) { //for deciding x axis coordinates, as j is the one used to loop through the x axis, and so holds the needed value
 				case 0:
 					tempCoordX = chunkCoordsWidthHeight[0] - chunkCoordsWidthHeight[2];
 					break;
@@ -260,7 +260,7 @@ void chunksHolder::updateChunks(sf::Sprite sprite1, int winWidth, int winHeight)
 					break;
 				}
 
-				chunksLoaded.push_back(
+				chunksLoaded.push_back( //adds the needed chunk to the chunksLoaded vector
 					chunks(
 						chunkCoordsWidthHeight[2],
 						chunkCoordsWidthHeight[3],
@@ -269,18 +269,18 @@ void chunksHolder::updateChunks(sf::Sprite sprite1, int winWidth, int winHeight)
 					)
 				);
 
-				surroundingChunks[i][j] = 1;
+				surroundingChunks[i][j] = 1; //once processing has finished, it is obviously present, so mark it as such in the array
 			}
 		}
 	}
 }
 
-int chunksHolder::chunkOverlap(int pointX, int pointY) {
-	int overlaps = 0;
+int chunksHolder::chunkOverlap(int pointX, int pointY) { //private function for checking of the overlap between the virtual points from earlier, and a chunk
+	bool overlaps = false; //initialise overlap to false
 
 	for (int i = 0; i < chunksLoaded.size(); i++) {
 		if (
-				( //check if a point in the top right virtual sector is in a chunk
+				( //check if a point in the top right virtual sector is in a chunk, literally normal intersection check
 				chunksLoaded[i].chunkCoords.x < pointX &&
 				chunksLoaded[i].chunkCoords.x + chunksLoaded[i].chunkWidthHeight.x > pointX &&
 				chunksLoaded[i].chunkCoords.y < pointY &&
@@ -288,11 +288,11 @@ int chunksHolder::chunkOverlap(int pointX, int pointY) {
 				)
 			)
 		{
-			overlaps++;
+			overlaps = true; //obviously if an overlap is present, set it to true
 		}
 	}
 
-	return overlaps;
+	return overlaps; //return the value of overlaps for the surroundingChunks[3][3] array
 }
 
 chunksHolder::~chunksHolder(){ //the destructor

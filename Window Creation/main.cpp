97425dc87669224bf9ac324ee45024bf323d280a
Ali Abbas <aliabbas.299792@gmail.com>
2019-01-02 17:06:36 +0000

    Did the Window creation over

--- Window Creation/main.cpp
+++ Window Creation/main.cpp
@@ -1,99 +1,50 @@
 #include <glad/glad.h>
 #include <glad/glad.c>
 #include <GLFW/glfw3.h>
-#include <fstream>
-#include <sstream>
-#include <vector>
-
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height); //for resize function
-void processInput(GLFWwindow *window);
+#include "header/functionDefines.h"
 
-int main() {
-	//////////////////////////////////////////////////////
-	//////////--------------------------------////////////
-	//////////------Window initialisation-----////////////
-	//////////--------------------------------////////////
-	//////////////////////////////////////////////////////
-
+int main(){
+	//glfw initialisation
 	glfwInit();
-
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(900, 700, "Window #1", NULL, NULL); //window creation
 
-	if (window == NULL) {
-		std::cout << "Failed to create window." << std::endl;
-
-		glfwTerminate();
-
-		return -1;
+	if(window == NULL){ //if creation failed
+		std::cout << "Window creation failed." << std::endl;
+		glfwTerminate(); //end
+		return -1; //ended in failure
 	}
 
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//otherwise we can continue processing
+	glfwMakeContextCurrent(window); 
+	//above makes the context of the window (so all of the states associated with the instance of OpenGL), current on the thread it was called from
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		system("PAUSE");
-		return -1;
-		//if glad does initialise
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ //initialises glad, and if the initialiser returns false, exit
+		std::cout << "Failed to initialise GLAD." << std::endl;
+		return -1; //ended in failue
 	}
 
+	glViewport(0, 0, 900, 700); //this is where everything is rendered inside of, params are: top x, top y, width, height respectively
 
-	//////////////////////////////////////////////////////
-	//////////--------------------------------////////////
-	//////////------Actual OpenGL Stuff-------////////////
-	//////////--------------------------------////////////
-	//////////////////////////////////////////////////////
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //call the function specified, when the specified window is resized
 
-	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
-	};
+	//the main loop
+	while(!glfwWindowShouldClose(window)){
+		processInput(window); //check if the escape key has been pressed
 
-	unsigned int VBO; //vertex buffer object id
-	unsigned int VAO;
+		glClearColor(0.2f,0.2f,0.2f,1.0f); //makes the entire screen this colour
+		glClear(GL_COLOR_BUFFER_BIT); //clears the colour buffer, to allow the colour from the above function to be displayed
 
-	glGenVertexArrays(1, &VAO);
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindVertexArray(VAO);
-
-	glEnableVertexAttribArray(0);
-
-	while (!glfwWindowShouldClose(window))
-	{
-
-		processInput(window);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		glfwSwapBuffers(window); //uses the double buffer thing, where the back buffer is drawn to and then swapped with the front one to prevent flickering
+		glfwPollEvents(); //checks for events and allows things such as the framebuffer_size_callback functions to be called once an event has been detected
 	}
 
+	//exit properly, first destroys all remaining windows, frees any allocated resources, and sets library to uninitialised state, before actually returning 0
 	glfwTerminate();
 	return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
 }

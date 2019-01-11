567fcc2319dcb67e984e45e19d7b846677ec0987
Ali Abbas <aliabbas.299792@gmail.com>
2019-01-11 19:36:24 +0000

    Delete functionDefines.h

--- Window Creation/header/functionDefines.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTION_DEFINES
-#define FUNCTION_DEFINES
-#include <GLFW/glfw3.h>
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height); //prototype function for resizing function
-void processInput(GLFWwindow* window); //for processing input, such as the escape key, to close the window
-#endif

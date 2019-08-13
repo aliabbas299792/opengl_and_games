31626fd4340fe8fade9457ee7171582da5409a7e
Ali Abbas <aliabbas.299792@gmail.com>
2019-08-13 02:22:28 +0100

    Refactored the code and put main bits into objects

---
+++ Game/Header/gui.h
@@ -0,0 +1,41 @@
+#ifndef GUI_HEADER
+#define GUI_HEADER
+
+#include <network.h>
+#include <TGUI/TGUI.hpp>
+
+#include <gui.h>
+
+class launcher {
+private:
+	sf::Time notifTime; //the amount of time a notification has been on screen (should be in milliseconds)
+	int loginError = 0; //0 = no error; 1 = error; 2 = error displayed, waiting to take it off
+	
+	sf::RenderWindow* window = NULL; //will hold the window
+
+	//will hold the 3 main threads
+	sf::Thread *pingThread = NULL;
+	sf::Thread *receiveThread = NULL;
+	sf::Thread *input = NULL;
+
+	//makes these 3 tgui widgets here rather than in the constructor as I need these in other functions as well
+	tgui::Label::Ptr unsuccessfulLabel = tgui::Label::create();
+	tgui::Panel::Ptr loginUnsuccessful = tgui::Panel::create();
+	tgui::Button::Ptr playButton = tgui::Button::create();
+
+	networking *networkBit = NULL; //will hold the networking object
+
+	sf::Clock *notifClock; //will hold the global clock
+
+	void loginFunctionFromWindow(tgui::EditBox::Ptr usernameBox, tgui::EditBox::Ptr passwordBox); //function prototype to check login
+public:
+	tgui::Gui *gui = NULL; //will be initialised later when the window object has been received, to hold the necessary widgets
+
+	void liveUpdate(); //the function to do stuff during the main loop
+	launcher(networking *networkObject, sf::RenderWindow* mainWindow, sf::Thread *ping, sf::Thread *receive, sf::Thread *inputThread, sf::Clock *globalClock);
+	//the constructor gets the necessary data from outside and stores references inside the object for use whenever
+
+	~launcher(); //the destructor, it deletes the gui which is made in the constructor
+};
+
+#endif // !GUI_HEADER
--- Game/main.cpp
+++ Game/main.cpp
@@ -1,36 +1,60 @@
 #include <iostream>
 #include <string>
-#include "SFML/Network.hpp"
 #include <functional>
+
+#include <SFML/Network.hpp>
+#include <TGUI/TGUI.hpp>
 
 //user defined header files
 #include <network.h>
+#include <gui.h>
 
-sf::Clock keepAliveTimer; //the clock which is used to check when to ping the server
-sf::Time pingTime; //the time at which a ping should occur
-sf::TcpSocket *socket = new sf::TcpSocket; //makes a new socket
-std::string username; //the name of this user
-std::string password; //password
-bool loggedIn = false; //whether or not the user successfully logged in
+//tgui lib selection
+#ifdef _DEBUG
+#	pragma comment(lib, "tgui-d.lib")
+#else
+#	pragma comment(lib, "tgui.lib")
+#endif
 
 int main() {
-	const std::string IPADDRESS("192.168.0.15"); //the IP address to connect to
-	const unsigned short PORT = 5000; //the port to connect to
+	sf::Clock* globalClock = new sf::Clock; //the clock which is used to check when to ping the server
+	networking* networkObject = new networking("192.168.0.15", 5000, globalClock);
 
+	sf::Thread* receiveThread = new sf::Thread(&networking::getResponses, networkObject); //make the getResponses() function run on this thread
+	sf::Thread* pingThread = new sf::Thread(&networking::stayAlive, networkObject); //makes the stayAlive() function run on this thread
+	sf::Thread* getInputThread = new sf::Thread(&networking::getInput, networkObject); //thread for getting user input
+
+	//window bit
+	sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(800, 600), "window", sf::Style::Close); //window
+	launcher* launcherObject = new launcher(networkObject, window, pingThread, receiveThread, getInputThread, globalClock); //launcher
+
+	while (window->isOpen())
+	{
+		sf::Event event;
+		while (window->pollEvent(event)) //check for events
+		{
+			if (event.type == sf::Event::Closed)
+				window->close(); //close the window when you find the close event basically
+
+			launcherObject->gui->handleEvent(event); // Pass the event to the widgets
+		}
+
+		window->clear(); //clears the previous contents of the screen off
+
+		launcherObject->liveUpdate(); //does the entire notifications bit as well as drawing widgets
+
+		window->display(); //the contents of the screen are shown
+
+		sf::sleep(sf::milliseconds(50)); //so the program doesnt just fry your CPU
+	}
+
+	//From here on, the TCP connection is severed, the threads are waited on and then destroyed, and then the network and launcher objects are deleted, and 0 is returned
 	std::string msg; //the string used in getInput(...)
-
-	sf::Thread* pingThread = 0; //thread for pinging the server
-	sf::Thread* receiveThread = 0; //thread for waiting for incoming messages
-
-	loginLoop(IPADDRESS, PORT, username, password, socket, loggedIn); //this will ask the user to login over and over
-
-	receiveThread = new sf::Thread(&getResponses, socket); //make the getResponses() function run on this thread
-	receiveThread->launch(); //launches the thread
-
-	pingThread = new sf::Thread(std::bind(&stayAlive, keepAliveTimer, pingTime, socket)); //makes the stayAlive() function run on this thread
-	pingThread->launch(); //launches the thread
-
-	getInput(msg, loggedIn, username, socket); //runs the getInput(...) function endlessly
+	sf::Packet sendPacket; //the packet which will contain the data to send
+	msg = "USER::LEAVE";
+	//this will help with decoding the data on the server side
+	sendPacket << msg.c_str(); //converts the string into a C style array, and puts it into the packet which will be sent
+	networkObject->socket->send(sendPacket); //sends the packet
 
 	if (pingThread) { //if the ping thread exists...
 		pingThread->wait(); //wait for it's current bit of code to finish
@@ -42,7 +66,14 @@ int main() {
 		delete receiveThread; //then delete it
 	}
 
-	delete socket;
+	if (getInputThread) {
+		getInputThread->wait();
+		delete getInputThread;
+	}
+
+	//when delete is called, the destructor is called before deallocating the memory
+	delete launcherObject;
+	delete networkObject;
 
 	return 0;
 }
